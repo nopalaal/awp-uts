341b167145fe67b4dc39a3382b3242ff0a54a10b
nopalaal <naufal.rabbani@student.umn.ac.id>
2025-10-12 21:38:40 +0700

    prtotype Read

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const app = express();
-const db = require('./koneksi')
 const path = require('path')
+
+const { user, sequelize } = require('./models');
+
 const port = 3000;
 
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 
-app.get('/',(req,res)=>{
-        res.render('index',{title :'home page'})
-
+app.get('/', async (req,res)=>{
+    try{
+        const users = await user.findAll();
+        res.render('index',{users});
+        console.log(users);
+    } catch(err){
+        res.status(500).send(err.message);
+    }
+    
 })
-db.authenticate()
-  .then(() => console.log('Dbnya connect'))
-  .catch(err => console.error('ga konek konek', err));
+sequelize.authenticate()
+.then(() => console.log('Dbnya connect'))
+.catch(err => console.error('ga konek konek', err));
 
 app.listen(port,()=>{
     console.log(`http://localhost:${port}`)
