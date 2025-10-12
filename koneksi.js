c700e6f7ef08cc33def2a8dbfce229bfaa854103
nopalaal <naufal.rabbani@student.umn.ac.id>
2025-10-12 21:57:15 +0700

    prototype create

--- koneksi.js
+++ koneksi.js
@@ -1,10 +1,10 @@
 const { Sequelize } = require('sequelize');
 
 
-const db = new Sequelize('mudjarap', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql', 
-  port: 3310,
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+  port: process.env.DB_PORT,
 });
 
 
