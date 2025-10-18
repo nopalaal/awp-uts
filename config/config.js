ab7b3f85a838f65ffd34d0a799c9db7d205a1b24
nopalaal <naufal.rabbani@student.umn.ac.id>
2025-10-19 00:41:36 +0700

    pls

--- config/config.js
+++ config/config.js
@@ -2,26 +2,27 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: "root",
-    password: "",
-    database: "mudjarap",
-    host: "localhost",
-    port: 3310,
-    dialect: "mysql"
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASS || "",
+    database: process.env.DB_NAME || "mudjarap",
+    host: process.env.DB_HOST || "localhost",
+    port: Number(process.env.DB_PORT) || 3310,
+    dialect: process.env.DB_DIALECT || "mysql"
   },
   test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASS || null,
+    database: process.env.DB_NAME_TEST || "database_test",
+    host: process.env.DB_HOST || "127.0.0.1",
+    port: Number(process.env.DB_PORT) || 3306,
+    dialect: process.env.DB_DIALECT || "mysql"
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3306,
+    port: Number(process.env.DB_PORT) || 3306,
     dialect: process.env.DB_DIALECT || "mysql",
     dialectOptions: {
       ssl: {
