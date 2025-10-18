b0fcb2fb7f7707ec74ee5d30560b94baeb993a37
nopalaal <naufal.rabbani@student.umn.ac.id>
2025-10-19 00:51:01 +0700

    liat console

--- src/models/koneksi.js
+++ src/models/koneksi.js
@@ -1,12 +1,11 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-// Centralize DB config via config.js
 const env = process.env.NODE_ENV || 'development';
 const config = require('../../config/config.js')[env];
-
 const db = new Sequelize(config.database, config.username, config.password, config);
 
+console.log('DB config:', config);
 // Optional: expose a quick test function if app.js expects it in the future
 async function testConnection() {
   try {
